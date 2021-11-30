a662960bea1cbf4001fce306a2588a98505345ec
jai.negi <jai.negi@IAPC--141.infoaxon.local>
2021-11-30 17:49:48 +0530

    added styling and api data output

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
 const handleClick = document.querySelector("#submit_form");
-handleClick.addEventListener("click",translate);
+handleClick.addEventListener("click", translate);
 
 //function to handle submit event when clicking on enter
 async function translate() {
-    console.log("it got called");
-    event.preventDefault();
-    const inputvalue = document.querySelector('#userInput').value;
+  console.log("it got called");
+  event.preventDefault();
+  const inputvalue = document.querySelector("#userInput").value;
 
-    const text = inputvalue.trim();
-    console.log(text);
+  const text = inputvalue.trim();
+  console.log(text);
 
-    try {
-         await fetch(`https://api.funtranslations.com/translate/minion.json?text=${text}`)
-            //Promises chaining
-         
-            .then(response => response.json())
-            .then(data => console.log(data.contents))          
-            .catch(err => console.error(err))
+  try {
+    await fetch(
+      `https://api.funtranslations.com/translate/minion.json?text=${text}`
+    )
+      //Promises chaining
 
-    } catch (err) {
-        console.log('error occured during excuting api call')
-    }
+      .then((response) => response.json())
+      .then((json) => {
+        var translatedMessage = json.contents.translate;
+        console.log("did you saw translated msg", translatedMessage);
+        document.querySelector("#textArea").innerText = translatedMessage;
+      })
+      .catch(console.error(err));
+  } catch (err) {
+    alert(
+      "api calls exceeded more then 5 in a minute, please try after 1 hour"
+    );
+  }
 }
-document.querySelector('#textArea').innerHTML
-
-
-
-
-
-
-
-
-// const form = document.querySelector('#submit_form');
-// form.addEventListener('submit', translate);
-
-
-// var text = document.querySelector("#userInput").value;
-
-// const url = `https://api.funtranslations.com/translate/minion.json${text}`;
-
-// function translate() {
-//   fetch(`https://api.funtranslations.com/translate/minion.json${text}`).then(
-//     console.log()
-//   );
-// }
-
-
-// //function to display values
-// function displayWeather(temp){
-//     document.getElementById('result-id').innerHTML = temp;
-// }
