c63e2b156b0beda47906033edd455241b200bc75
jai.negi <jai.negi@IAPC--141.infoaxon.local>
2021-12-05 14:23:13 +0530

    minor fix

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
 const handleClick = document.querySelector("#submit_form");
-handleClick.addEventListener("click", translate);
 
 //function to handle submit event when clicking on enter
-async function translate() {
-  console.log("it got called");
-  event.preventDefault();
-  const inputvalue = document.querySelector("#userInput").value;
-
-  const text = inputvalue.trim();
-  console.log(text);
-
-  try {
-    await fetch(
-      `https://api.funtranslations.com/translate/minion.json?text=${text}`
-    )
-      //Promises chaining
-
-      .then((response) => response.json())
-      .then((json) => {
-        var translatedMessage = json.contents.translate;
-        console.log("did you saw translated msg", translatedMessage);
-        document.querySelector("#textArea").innerText = translatedMessage;
-      })
-      .catch(console.error(err));
-  } catch (err) {
-    alert(
-      "api calls exceeded more then 5 in a minute, please try after 1 hour"
-    );
-  }
+function errorHandler(error) {
+  console.log("error occured", error.message);
+  alert("api calls exceeds more then 5 in an hour", error);
 }
+
+function translate() {
+  const inputvalue = document.querySelector("#userInput").value;
+  const text = inputvalue.trim();
+  var outputDiv = document.querySelector("#textArea");
+
+  fetch(`https://api.funtranslations.com/translate/minion.json?text=${text}`)
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedMessage = json.contents.translated;
+      console.log("did you saw translated msg", translatedMessage);
+      outputDiv.innerText = translatedMessage;
+    })
+    .catch(errorHandler);
+}
+handleClick.addEventListener("click", translate);
